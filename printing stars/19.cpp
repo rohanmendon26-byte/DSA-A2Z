1414788762d91cf68d1f2ba7e6f17966df8cb707
Rohan Mendon <rohanmendon26@gmail.com>
2026-02-20 21:19:48 +0530

    printing stars added

--- printing stars/19.cpp
+++ printing stars/19.cpp
@@ -1,11 +1,60 @@
 #include<iostream>
 using namespace std;
 
-void main(){
-    int 
+void print1(int n){
+    int inis=0;
+    for(int i=0;i<n;i++){
+        //stars
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j=0;j<inis;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        inis+=2;
+        cout<<endl;
+    }  
+}
+
+void print2(int n){
+    int inis=8;
+    for(int i=1;i<=n;i++){
+        //stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+
+        //spaces
+        for(int j=1;j<=inis;j++){
+            cout<<" ";
+        }
+
+        //stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        inis-=2;
+        cout<<endl;
+    }
 }
 int main()
 {
-    
-    return 0;
+    print1(5);
+    print2(5);
 }
+
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
