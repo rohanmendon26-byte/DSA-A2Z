e4d78489fc666c15b5b2ef03893846b3c3364786
Rohan Mendon <rohanmendon26@gmail.com>
2026-02-27 21:16:54 +0530

    Basic maths added

--- Basic Maths/mat.cpp
+++ Basic Maths/mat.cpp
@@ -141,3 +141,49 @@ int* printDivisors(int n, int &size){
 }
 //******************************************
 
+//Prime number
+int main() {
+	// Write your code here
+	int n;
+	cin>>n;
+	int cnt=0;
+	for(int i=1;i*i<=n;i++){
+		if(n%i==0){
+			cnt++;
+			if((n/i)!=i)
+			   cnt++;
+		}	
+	}
+	if(cnt==2){
+		cout<<"true";
+	}
+	else{
+		cout<<"false";
+	}
+}
+
+//******************************************
+
+//GCD
+int main() {
+	// Write your code here
+	int n;
+	cin>>n;
+	int cnt=0;
+	for(int i=1;i*i<=n;i++){
+		if(n%i==0){
+			cnt++;
+			if((n/i)!=i)
+			   cnt++;
+		}	
+	}
+	if(cnt==2){
+		cout<<"true";
+	}
+	else{
+		cout<<"false";
+	}
+}
+
+
+
