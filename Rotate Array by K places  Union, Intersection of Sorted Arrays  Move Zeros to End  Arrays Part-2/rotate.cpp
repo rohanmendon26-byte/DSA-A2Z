652664019da598235adf9d40dd0b7d53ea16c270
Rohan Mendon <rohanmendon26@gmail.com>
2026-03-26 21:48:00 +0530

    completed

--- Rotate Array by K places  Union, Intersection of Sorted Arrays  Move Zeros to End  Arrays Part-2/rotate.cpp
+++ Rotate Array by K places  Union, Intersection of Sorted Arrays  Move Zeros to End  Arrays Part-2/rotate.cpp
@@ -66,3 +66,49 @@ public:
 
     }
 };
+
+
+//move zeros to the end of the array (BRUTE FORCE)
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> temp;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)
+              temp.push_back(nums[i]);
+        }
+
+        for(int i=0;i<temp.size();i++){
+            nums[i]=temp[i];
+        }
+
+        for(int i=temp.size();i<n;i++){
+            nums[i]=0;
+        }
+    }
+};
+
+
+//move zeros to the end of the array (OPTIMAL)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int j=-1;
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            j=i;
+            break;
+        }
+       
+    }
+
+    for(int i=j+1;i<n;i++){
+        if(nums[i]!=0){
+            swap(nums[i],nums[j])
+            j++;
+        }
+    }
+};
