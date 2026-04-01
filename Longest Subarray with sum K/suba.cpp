5180791c5f043c236471ca6320df8a351e9e2f15
Rohan Mendon <rohanmendon26@gmail.com>
2026-04-01 06:24:37 +0530

    completed

--- Longest Subarray with sum K/suba.cpp
+++ Longest Subarray with sum K/suba.cpp
@@ -27,7 +27,7 @@ int longestSubarrayWithSumK(vector<int> a, long long k) {
         if(sum==k){
             maxlen=max(maxlen,i+1);
         }
-        int rem=sum-k;
+        long long rem=sum-k;
         if(presummap.find(rem)!=presummap.end()){
             int len=i-presummap[rem];
             maxlen=max(maxlen,len);
@@ -37,4 +37,35 @@ int longestSubarrayWithSumK(vector<int> a, long long k) {
         }
     }
     return maxlen;
+}
+
+
+
+//Longest Subarray (Optimal approach)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int left=0,right=0;
+    long long sum=a[0];
+    int n=a.size();
+    int maxlen=0;
+    while(right<n){
+        while(left<=right && sum>k){
+            sum=sum-a[left];
+            left++;
+        }
+        if(sum==k){
+           maxlen=max(maxlen,right-left+1);
+        }
+
+        right++;
+        if(right<n){
+            sum=sum+a[right];
+        }
+    }
+    return maxlen;
+   
 }
