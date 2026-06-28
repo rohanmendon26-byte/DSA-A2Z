72de743e6cbb7b95dc4ea9037bb100096f032e81
Rohan Mendon <rohanmendon26@gmail.com>
2026-06-28 08:02:38 +0530

    completed

--- Arrays/Hard/3 Sum/3sum.cpp
+++ Arrays/Hard/3 Sum/3sum.cpp
@@ -24,3 +24,64 @@ vector<vector<int>> triplet(int n, vector<int> &arr)
 // Output: 
 // -1 -1 2
 // -1 0 1
+
+
+//bettter approach (O(n2))
+#include<bits/stdc++.h>
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    set<vector<int>> st;
+    for(int i=0;i<n;i++){
+            set<int>hashset;
+        for(int j=i+1;j<n;j++){
+            int third=-(arr[i]+arr[j]);
+            if(hashset.find(third) != hashset.end()){
+                vector<int>temp={arr[i],arr[j],third};
+                sort(temp.begin(),temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+                }
+            }
+    vector<vector<int>> ans(st.begin(),st.end());
+    return ans;
+}
+
+// input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
+// Input: nums = [0,1,1]
+// Output: []
+
+
+//optimal solution O(n2)
+#include<bits/stdc++.h>
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    vector<vector<int>> ans;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if(i>0 && arr[i]== arr[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+            if(sum > 0){
+                k--;
+            }
+            else if(sum<0){
+                j++;
+            }
+            else{
+                vector<int>temp={arr[i],arr[j],arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1]) j++;
+                while(j<k && arr[k]==arr[k+1]) k--;
+            }
+        }
+    }
+    return ans;
+}
+
