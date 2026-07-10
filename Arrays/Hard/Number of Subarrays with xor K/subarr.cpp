c908928307753a045958d8326f528058e9aa183a
Rohan Mendon <rohanmendon26@gmail.com>
2026-07-10 17:20:29 +0530

    completed

---
+++ Arrays/Hard/Number of Subarrays with xor K/subarr.cpp
@@ -0,0 +1,28 @@
+//X=XR^K
+
+#include <bits/stdc++.h>
+
+int subarraysXor(vector<int> &arr, int k)
+{
+    map<int,int>mpp;
+    int n=arr.size();
+    int xr=0;
+    mpp[xr]++;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        xr=xr^arr[i];
+        int x=xr^k;
+        cnt+=mpp[x];
+        mpp[xr]++;
+    }
+    return cnt;
+}
+
+// Input: arr[] = [4, 2, 2, 6, 4], k = 6
+// Output: 4
+// Explanation: The subarrays having XOR of their elements as 6 are [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], and [6]. Hence, the answer is 4.
+
+
+// Input: arr[] = [5, 6, 7, 8, 9], k = 5
+// Output: 2
+// Explanation: The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]. Hence, the answer is 2.
