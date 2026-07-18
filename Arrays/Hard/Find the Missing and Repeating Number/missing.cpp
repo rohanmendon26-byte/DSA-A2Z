1382c85bb7f8f155a5740669e779cafad43b48fb
Rohan Mendon <rohanmendon26@gmail.com>
2026-07-18 21:44:22 +0530

    completed

--- Arrays/Hard/Find the Missing and Repeating Number/missing.cpp
+++ Arrays/Hard/Find the Missing and Repeating Number/missing.cpp
@@ -38,3 +38,34 @@ pair<int,int> missingAndRepeating(vector<int> &arr, int n)
 // Explanation: Repeating number is 1 and the missing number is 5.
 
 
+
+//OPTIMAL APPROACH (MATHEMATICS METHOD)
+// Time Complexity
+// O(n)
+// Space Complexity
+// O(1)
+
+#include <bits/stdc++.h>
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	long long N=arr.size();
+	//S - Sn = x - y
+	//S2 - S2N
+	long long SN=(n*(n+1))/2;
+	long long S2N=(n*(n+1)*(2*n+1))/6;
+	long long S=0,S2=0;
+	for(int i=0;i<n;i++){
+		S+=arr[i];
+		S2+= (long long)arr[i] * (long long)arr[i];
+	}
+	long long val1 = S-SN;
+	long long val2 = S2-S2N;
+	val2=val2/val1;
+	long long x =(val1+val2)/2;
+	long long y = x-val1;
+	return {(int)y,(int)x};
+	
+}
+
+
+
