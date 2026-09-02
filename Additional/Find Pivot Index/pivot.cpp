3d141dff9bd7b0558fc8b14b2624d66209810a9f
Rohan Mendon <rohanmendon26@gmail.com>
2026-09-02 22:05:13 +0530

    completed

---
+++ Additional/Find Pivot Index/pivot.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalsum=0;
+        int n=nums.size();
+        for(int num:nums){
+            totalsum+=num;
+        }
+
+        int leftsum=0;
+        for(int i=0;i<n;i++){
+            int rightsum=totalsum-leftsum-nums[i];
+
+            if(leftsum==rightsum)
+               return i;
+            
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
+
+
+// Example 1:
+
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+// Example 2:
+
+// Input: nums = [1,2,3]
+// Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
+// Example 3:
+
+// Input: nums = [2,1,-1]
+// Output: 0
+// Explanation:
+// The pivot index is 0.
+// Left sum = 0 (no elements to the left of index 0)
+// Right sum = nums[1] + nums[2] = 1 + -1 = 0
