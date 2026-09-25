d552133bd0ccab3e5c4a61966c48d183eef1aa6e
Rohan Mendon <rohanmendon26@gmail.com>
2026-09-25 05:58:27 +0530

    updated

---
+++ Linked List/Medium Problems of LL/Remove Nth Node From End of List/back.cpp
@@ -0,0 +1,66 @@
+// So:
+
+// O(n) + O(L) = O(L)
+
+// because n ≤ L.
+
+// Space Complexity: O(1)
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+
+        if(fast==NULL){
+            return head->next;
+        }
+
+
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+        }
+
+    
+        ListNode *DelNode=slow->next;
+        slow->next=slow->next->next;
+        delete DelNode;
+
+        return head;
+    }
+};
+
+
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
